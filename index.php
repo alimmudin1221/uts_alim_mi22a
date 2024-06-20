7019b7feebc1585312075dbebfc83a245685fbc3
alimmudin1221 <158237196+alimmudin1221@users.noreply.github.com>
2024-06-20 14:10:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<style>
+    #menu {
+        margin: 0;
+        padding: 0;
+    }
+
+    #menu a {
+        text-decoration: none;
+        color: black;
+    }
+
+    #menu li {
+        display: inline-block;
+        border: 1px solid gray;
+        padding: 8px;
+    }
+
+    #menu li:hover {
+        background-color: gray;
+    }
+</style>
+
+<ul id="menu">
+    <li><a href="">Beranda</a></li>
+    <li><a href="login.php">FormLogin</a></li>
+    <li><a href="NIM.loop.php">Formloop </a></li>
+    <li><a href="profil.php">FormProfile</a></li>
+</ul>
+
